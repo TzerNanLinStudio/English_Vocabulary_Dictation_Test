5b49b23852178fea47a9571516bdd81c7bf4752c
LarryLin35 <124746278+LarryLin35@users.noreply.github.com>
2024-11-25 20:02:53 +0000

    testing

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestAddMethod()
+        {
+            // Arrange
+            var expected = 5;
+            var a = 2;
+            var b = 3;
+
+            // Act
+            var result = Add(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
