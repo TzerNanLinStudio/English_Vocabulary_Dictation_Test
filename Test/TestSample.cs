7e7a52e255ab4b2f7dfd99d89717cd00f1b61112
LarryLin35 <124746278+LarryLin35@users.noreply.github.com>
2024-11-29 00:13:01 +0000

    add some tests

---
+++ Test/TestSample.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class TestSample
+    {
+        // Test for Add method (int)
+        [TestMethod]
+        public void TestAddMethod_Int()
+        {
+            // Arrange
+            var expected = 5;
+            var a = 2;
+            var b = 3;
+
+            // Act
+            var result = Add(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // Test for And method (bool)
+        [TestMethod]
+        public void TestAndMethod_Bool()
+        {
+            // Arrange
+            var expected = true;
+            var a = true;
+            var b = true;
+
+            // Act
+            var result = And(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        public bool And(bool a, bool b)
+        {
+            return a && b;
+        }
+
+        // Test for Concat method (char)
+        [TestMethod]
+        public void TestConcatMethod_Char()
+        {
+            // Arrange
+            var expected = "ab";
+            var a = 'a';
+            var b = 'b';
+
+            // Act
+            var result = Concat(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        public string Concat(char a, char b)
+        {
+            return a.ToString() + b.ToString();
+        }
+
+        // Test for Combine method (string)
+        [TestMethod]
+        public void TestCombineMethod_String()
+        {
+            // Arrange
+            var expected = "hello world";
+            var a = "hello";
+            var b = " world";
+
+            // Act
+            var result = Combine(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        public string Combine(string a, string b)
+        {
+            return a + b;
+        }
+
+        // Test for Add method (double)
+        [TestMethod]
+        public void TestAddMethod_Double()
+        {
+            // Arrange
+            var expected = 5.5;
+            var a = 2.2;
+            var b = 3.3;
+
+            // Act
+            var result = Add(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, result, 0.0001); // Delta for floating-point precision
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Test/VocaburaryCoreTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VocaburaryCore;
+
+namespace Test
+{
+    [TestClass]
+    public class VocaburaryCoreTest
+    {
+        [TestMethod]
+        public void IntDateTest()
+        {
+            VocaburaryManagement testObject = new VocaburaryManagement();
+            Assert.AreEqual(true, testObject.WhetherDateCorrect(20201212));
+            Assert.AreEqual(false, testObject.WhetherDateCorrect(20201412));
+            Assert.AreEqual(false, testObject.WhetherDateCorrect(20201242));
+            Assert.AreEqual(false, testObject.WhetherDateCorrect(20206666));
+            Assert.AreEqual(true, testObject.WhetherDateCorrect(20200229));
+            Assert.AreEqual(false, testObject.WhetherDateCorrect(20220229));
+        }
+    }
+}
